9585790609db0de659bdb9c13cc41070149a185d
William Johansson <william.johansson98@gmail.com>
2019-01-01 12:23:33 +0100

    Added support for decimal numbers

--- main.js
+++ main.js
@@ -5,19 +5,44 @@ display.value = '';
 
 let displayString = '';
 
+// Add buttons 1-9
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
         let button = document.createElement('button');
         button.textContent = j + 1 + 3 * i;
         button.style.gridColumnStart = j + 1;
         button.style.gridRowStart = i + 1;
-        button.style.flexGrow = 1;
         button.classList.add('calcButton');
 
         container.appendChild(button);
     }
 }
 
+// Add zero button
+let button = document.createElement('button');
+button.textContent = 0;
+button.style.gridColumnStart = 2;
+button.style.gridRowStart = 4;
+button.classList.add('calcButton');
+container.appendChild(button);
+
+// Add '.' button
+button = document.createElement('button');
+button.textContent = '.';
+button.style.gridColumnStart = 1;
+button.style.gridRowStart = 4;
+button.classList.add('calcButton');
+container.appendChild(button);
+
+// Add clear button
+button = document.createElement('button');
+button.textContent = 'Clear';
+button.style.gridColumnStart = 3;
+button.style.gridRowStart = 4;
+button.classList.add('clearButton');
+container.appendChild(button);
+
+// Add actionlisteners for all buttons
 let buttons = document.querySelectorAll('.calcButton');
 buttons.forEach(button => {
     button.addEventListener('click' ,e => {        
@@ -26,11 +51,13 @@ buttons.forEach(button => {
     });
 });
 
+// Add actionlistener for clear button
 document.querySelector('.clearButton').addEventListener('click', e => {
     displayString = '';
     display.value = displayString;
 });
 
+// Add actionlistener for equals button
 document.querySelector('.equalsButton').addEventListener('click', e => {
     let output = calculate();
     display.value = output;
@@ -43,10 +70,11 @@ function calculate() {
     let numbers = [];
     let operators = [];
 
+    // Get the numbers in one array and the operators in another.
     let lastOperator = -1;
     let currentNumber = '';
     for (let i = 0; i < displayString.length; i++) {
-        if (Number(displayString[i])) {
+        if (Number(displayString[i]) || displayString[i] == '.' || displayString[i] == '0') {
             currentNumber = currentNumber.concat(displayString[i]);
         } else {
             numbers.push(currentNumber);
@@ -56,14 +84,50 @@ function calculate() {
     }
     numbers.push(currentNumber);
 
-    let answer = 0;
+    // Calculate the answer
+    let answer = calculateAnswer(operators, numbers);
+    let formatAnswer = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 10 }).format(answer)
+    return formatAnswer;
+}
+
+function calculateAnswer(operators, numbers) {
+    // First all '*'
     for (let i = 0; i < operators.length; i++) {
-        answer += operate(numbers[i], numbers[i+1], operators[i]);
+        if (operators[i] == '*') {
+            numbers[i] = operate(numbers[i],  numbers[i+1], operators[i]); 
+            numbers.splice(i + 1, 1);
+            operators.splice(i, 1);
+        }
     }
 
-    console.log(numbers, operators);
-    
-    return answer;
+    // Then all '/'
+    for (let i = 0; i < operators.length; i++) {
+        if (operators[i] == '/') {
+            numbers[i] = operate(numbers[i],  numbers[i+1], operators[i]); 
+            numbers.splice(i + 1, 1);
+            operators.splice(i, 1);
+        }
+    }
+
+    // Then all '-'
+    for (let i = 0; i < operators.length; i++) {
+        if (operators[i] == '-') {
+            numbers[i] = operate(numbers[i],  numbers[i+1], operators[i]); 
+            numbers.splice(i + 1, 1);
+            operators.splice(i, 1);
+        }
+    }
+
+    // Then all '+'
+    for (let i = 0; i < operators.length; i++) {
+        if (operators[i] == '+') {
+            numbers[i] = operate(numbers[i],  numbers[i+1], operators[i]); 
+            numbers.splice(i + 1, 1);
+            operators.splice(i, 1);
+        }
+    }
+
+    return numbers[0];
 }
 
 function checkForErrors() {
@@ -85,12 +149,21 @@ function checkForErrors() {
 function getDisplayValue() {
     let string = '';
     for (let i = 0; i < displayString.length; i++) {
-        if (Number(displayString[i])) {
+        if (Number(displayString[i]) || displayString[i] == '.' || displayString[i] == '0') {
             string = string.concat(displayString[i]);
         } else {
             string = string.concat(' ' + displayString[i] +  ' ');
         }
     }
+
+    // If it is too long decrease the font-size.
+    if (string.length >= 10) {
+        let input = document.querySelector('.display');
+        console.log("Tried to decrease font-size");
+        input.style.classList.add('smallFont');
+        //input.style.fontSize = 10;
+    }
+
     return string;
 }
 
